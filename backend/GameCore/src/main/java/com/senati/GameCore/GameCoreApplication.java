68ab849fccff32b7a16ea76808f9e80a103b9e68
G0nch0TEC <benzaquenrios@gmail.com>
2026-03-28 05:10:56 -0500

    chore: estructura inicial del proyecto con frontend y backend

---
+++ backend/GameCore/src/main/java/com/senati/GameCore/GameCoreApplication.java
@@ -0,0 +1,13 @@
+package com.senati.GameCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameCoreApplication.class, args);
+	}
+
+}
---
+++ backend/GameCore/src/test/java/com/senati/GameCore/GameCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senati.GameCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
